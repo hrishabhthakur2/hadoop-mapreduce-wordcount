a9e8d4bc8c59006f849f6b5a96d7dbc541dc14e2
hrishabhthakur2 <rishabh12ka4@gmail.com>
2026-08-15 18:11:21 +0000

    Add WordCount output and execution timing

--- WordCount.java
+++ WordCount.java
@@ -1,82 +1,76 @@
 import java.io.IOException;
+import java.util.StringTokenizer;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
-
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
-
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class WordCount {
 
-    // Mapper
-    public static class TokenizerMapper
-            extends Mapper<Object, Text, Text, IntWritable> {
+  public static class TokenizerMapper
+       extends Mapper<Object, Text, Text, IntWritable>{
 
-        private final static IntWritable ONE = new IntWritable(1);
-        private Text word = new Text();
+    private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
 
-        @Override
-        public void map(Object key, Text value, Context context)
-                throws IOException, InterruptedException {
+    public void map(Object key, Text value, Context context
+                    ) throws IOException, InterruptedException {
+      String line = value.toString().replaceAll("[^a-zA-Z0-9 ]", "");
+      StringTokenizer itr = new StringTokenizer(line);
 
-            String[] words = value.toString().split("\\s+");
-
-            for (String w : words) {
-                word.set(w);
-                context.write(word, ONE);
-            }
-        }
+      while (itr.hasMoreTokens()) {
+        word.set(itr.nextToken());
+        context.write(word, one);
+      }
     }
+  }
 
-    // Reducer
-    public static class IntSumReducer
-            extends Reducer<Text, IntWritable, Text, IntWritable> {
+  public static class IntSumReducer
+       extends Reducer<Text,IntWritable,Text,IntWritable> {
+    private IntWritable result = new IntWritable();
 
-        private IntWritable result = new IntWritable();
-
-        @Override
-        public void reduce(Text key, Iterable<IntWritable> values,
-                           Context context)
-                throws IOException, InterruptedException {
-
-            int sum = 0;
-
-            for (IntWritable value : values) {
-                sum += value.get();
-            }
-
-            result.set(sum);
-            context.write(key, result);
-        }
+    public void reduce(Text key, Iterable<IntWritable> values,
+                       Context context
+                       ) throws IOException, InterruptedException {
+      int sum = 0;
+      for (IntWritable val : values) {
+        sum += val.get();
+      }
+      result.set(sum);
+      context.write(key, result);
     }
+  }
 
-    // Driver
-    public static void main(String[] args)
-            throws Exception {
+  public static void main(String[] args) throws Exception {
+    Configuration conf = new Configuration();
+    Job job = Job.getInstance(conf, "word count");
 
-        Configuration conf = new Configuration();
+    job.getConfiguration().setLong(
+	"mapreduce.input.fileinputformat.split.maxsize", 1000000);
 
-        Job job = Job.getInstance(conf, "word count");
+    job.setJarByClass(WordCount.class);
+    job.setMapperClass(TokenizerMapper.class);
+    job.setCombinerClass(IntSumReducer.class);
+    job.setReducerClass(IntSumReducer.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
-        job.setJarByClass(WordCount.class);
+    long sT = System.currentTimeMillis();
 
-        job.setMapperClass(TokenizerMapper.class);
-        job.setCombinerClass(IntSumReducer.class);
-        job.setReducerClass(IntSumReducer.class);
+    boolean isSuccessful = job.waitForCompletion(true);
 
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(IntWritable.class);
+    long eT = System.currentTimeMillis();
 
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+    System.out.println("Execution Time: " + (eT-sT));
 
-        System.exit(job.waitForCompletion(true) ? 0 : 1);
-    }
+    System.exit(isSuccessful ? 0 : 1);
+  }
 }
